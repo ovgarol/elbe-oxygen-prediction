3f21bc49ebed6986bbcec08e3e8681f601ca609c
Ovidio García-Oliva <ovidiogarciaoliva@gmail.com>
2025-11-26 16:32:53 +0100

    Add files via upload

---
+++ run_all.R
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Helmholtz-Zentrum hereon GmbH
+# SPDX-License-Identifier: CC-BY-4.0
+# SPDX-FileContributor Ovidio Garcia-Oliva <ovidio.garcia@hereon.de>
+ 
+system('mkdir -p fig')
+source('lagged_model.R')
+source('ews.R')
