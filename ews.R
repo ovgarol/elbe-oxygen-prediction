c6755b3999bc2726d654f9de5485df831d5ba007
Ovidio García-Oliva <ovidiogarciaoliva@gmail.com>
2026-01-08 13:39:09 +0100

    Refactor mask.far and streamline predictions
    Refactored mask.far function and updated predictions handling.

--- ews.R
+++ ews.R
@@ -2,8 +2,12 @@
 # SPDX-License-Identifier: CC-BY-4.0
 # SPDX-FileContributor Ovidio Garcia-Oliva <ovidio.garcia@hereon.de>
 
-mask.far <- function(M, N, off) {
+library(matrixStats)
+
+mask.far = function(M, N, off) {
   idx = abs(row(M) - col(M) + off) >= N
+  idx[,1:N] = F 
+  print(idx)
   M[idx] = NA
   return(M)
 }
@@ -58,47 +62,107 @@ for(i in 1:NN.max){
   predictions.hypox[,i] = 100*predicted_hypoxia
 }
 
-
-
-predictions.DO = mask.far(predictions.DO,2*observation.window,-length(wgmn_data$t)+NN.max)
-predictions.hypox = mask.far(predictions.hypox,2*observation.window,-length(wgmn_data$t)+NN.max)
-
-
+predictions.DO = mask.far(predictions.DO,1*observation.window,-length(wgmn_data$t)+NN.max)
+predictions.hypox = mask.far(predictions.hypox,1*observation.window,-length(wgmn_data$t)+NN.max)
 
 wgmn_data$pred.DO = rowMeans(predictions.DO,na.rm = T)
 wgmn_data$pred.hypox = rowMeans(predictions.hypox,na.rm = T)
 
+sigma.level = 3 # amplification factor for 1-2-3-sigma levels following the 68-95-99.7 rule of the sample mean
+wgmn_data$pred.DO.error = sigma.level*apply(predictions.DO, 1, function(...)sd(...,na.rm=T)*(1/(sum(!is.na(...))))**0.5)
+wgmn_data$pred.hypox.error = sigma.level*apply(predictions.hypox, 1, function(...)sd(...,na.rm=T)*(1/(sum(!is.na(...))-1))**0.5)
+
+wgmn_data$pred.DO.error[is.na(wgmn_data$pred.DO.error)] = 1.5*max(wgmn_data$pred.DO.error,na.rm = T) 
+wgmn_data$pred.hypox.error[is.na(wgmn_data$pred.hypox.error)] = 1.5*max(wgmn_data$pred.hypox.error,na.rm = T)  
 
 pdf('prediction.pdf',width=8,height = 4)
 par(mfrow=c(1,2),las=1)
 
 plot(wgmn_data$t,wgmn_data$z,
      xlim=c(max.date-14,max(wgmn_data$t)),
-     ylim=range(c(wgmn_data$z,wgmn_data$pred.DO),na.rm=T),
+     ylim=range(predictions.DO[(length(wgmn_data$t)-2*NN.max):nrow(predictions.DO),],na.rm=T),
+     #ylim=range(c(wgmn_data$z,wgmn_data$pred.DO+2),na.rm=T),
      xlab='',
      ylab='mg/L',
-     type='l',lwd=2
+     pch=20,
+     cex=0.5,
+     type='o',lwd=1
      )
 title(main='dissolved oxygen',adj=0)
 abline(v=max.date+(0:4)*7,col='lightgray')
 lines(wgmn_data$t,wgmn_data$pred.DO,col='red')
+
+polygon(c(wgmn_data$t,rev(wgmn_data$t))[c(!is.na(wgmn_data$pred.DO.error),rev(!is.na(wgmn_data$pred.DO.error)))],
+        c(wgmn_data$pred.DO-wgmn_data$pred.DO.error,rev(wgmn_data$pred.DO+wgmn_data$pred.DO.error))[c(!is.na(wgmn_data$pred.DO.error),rev(!is.na(wgmn_data$pred.DO.error)))],
+        col=alpha('tomato',0.25),
+        border=F
+        )
+
 lines(wgmn_data$t,apply(predictions.DO, 1, function(...)min(...,na.rm=T)),col='red',lty=3)
 lines(wgmn_data$t,apply(predictions.DO, 1, function(...)max(...,na.rm=T)),col='red',lty=3)
-lines(supsmu(as.Date('2025-01-01')+yday(df$t)-1,df$z,span = 0.1/365),col='orange')
+#lines(supsmu(as.Date('2025-01-01')+yday(df$t)-1,df$z,span = 0.01/365),col='orange')
+
+y.pos = range(predictions.DO[(length(wgmn_data$t)-2*NN.max):nrow(predictions.DO),],na.rm=T)[2]
+
+points(wgmn_data$t,
+       0*wgmn_data$pred.DO + y.pos,
+       col=alpha(hcl.colors(10,'Zi',rev=T),0.995)[1+as.integer(pmin(10,pmax(0,10-10*(wgmn_data$pred.DO.error))))],
+       cex=1.25,#3-2*(wgmn_data$pred.DO.error),
+       type='p',
+       pch=15
+       )
+
+if(T)for(i in 1:length(wgmn_data$t))if(i%%2==0){
+  text(wgmn_data$t[i],
+       y.pos,
+       9-round(10*(wgmn_data$pred.DO.error)[i],digits = 0),
+       las=2,
+       cex=0.5
+       )
+}
 
 plot(wgmn_data$t,100*wgmn_data$hypoxia,
      xlim=c(max.date-14,max(wgmn_data$t)),
-     ylim=c(0,15),
+     ylim=c(0.01,max(wgmn_data$pred.hypox,na.rm = T)),
      xlab='',
+     log='y',
      ylab='%',
      type='l',lwd=2
 )
 title(main='hypoxia probability',adj=0)
 abline(v=max.date+(0:4)*7,col='lightgray')
 lines(wgmn_data$t,wgmn_data$pred.hypox,col='red')
+
+polygon(c(wgmn_data$t,rev(wgmn_data$t))[c(!is.na(wgmn_data$pred.DO.error),rev(!is.na(wgmn_data$pred.DO.error)))],
+        pmax(0.001, c(wgmn_data$pred.hypox-wgmn_data$pred.hypox.error,rev(wgmn_data$pred.hypox+wgmn_data$pred.hypox.error))[c(!is.na(wgmn_data$pred.DO.error),rev(!is.na(wgmn_data$pred.DO.error)))]),
+        col=alpha('tomato',0.25),
+        border=F
+        )
+
 lines(wgmn_data$t,apply(predictions.hypox, 1, function(...)max(...,na.rm=T)),col='red',lty=3)
 lines(wgmn_data$t,apply(predictions.hypox, 1, function(...)min(...,na.rm=T)),col='red',lty=3)
-lines(supsmu(as.Date('2025-01-01')+yday(df$t)-1,as.numeric(df$hypoxia),span = 0.1/365),col='orange')
+
+#lines(supsmu(as.Date('2025-01-01')+yday(df$t)-1,as.numeric(df$hypoxia),span = 0.1/365),col='orange')
+
+y.pos = max(wgmn_data$pred.hypox,na.rm = T)
+
+points(wgmn_data$t,
+       0*wgmn_data$pred.DO + y.pos,
+       col=alpha(hcl.colors(10,'Zi',rev=T),0.995)[1+as.integer(pmin(10,pmax(0,10-10*(wgmn_data$pred.hypox.error))))],
+       cex=1.25,#3-2*(wgmn_data$pred.DO.error),
+       type='p',
+       pch=15
+)
+
+if(T)for(i in 1:length(wgmn_data$t))if(i%%2==0){
+  text(wgmn_data$t[i],
+       y.pos,
+       9-round(10*(wgmn_data$pred.hypox.error)[i],digits = 0),
+       las=2,
+       cex=0.5
+  )
+}
+
 
 ## matrix plot
 image(wgmn_data$t,1:NN.max,predictions.DO,
@@ -122,3 +186,5 @@ axis(side=1,at=xlabs,label=paste0(month(xlabs),'-',day(xlabs)),las=1)
 abline(v=max.date+(0:4)*7)
 
 dev.off()
+
+
